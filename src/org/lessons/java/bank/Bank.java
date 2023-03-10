535e26bf37ebb7d99be4f048bf9c725e8e3c0130
LuisJann <giannoneluigi10@gmail.com>
2023-03-10 17:10:14 +0100

    Add interaction with user in Product & format price

--- src/org/lessons/java/bank/Bank.java
+++ src/org/lessons/java/bank/Bank.java
@@ -1,9 +1,11 @@
 package org.lessons.java.bank;
 
+import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class Bank {
     public static void main(String[] args) {
+        DecimalFormat df = new DecimalFormat("###.00");
         Scanner scan = new Scanner(System.in);
         Account account = new Account();
         boolean check = true;
@@ -22,22 +24,22 @@ public class Bank {
 
             switch (userChoose) {
                 case 1 -> {
-                    System.out.println("Il tuo saldo attuale è: " + account.getBalance() + "€");
+                    System.out.println("Il tuo saldo attuale è: " + df.format(account.getBalance()) + "€");
                     System.out.println("Quanto vuoi versare?");
                     account.setBalance(Integer.parseInt(scan.nextLine()));
                     System.out.println("Caricamento in corso...");
                     System.out.println("Il tuo saldo è stato aggiornato");
-                    System.out.println("Il saldo disponibile ora è: " + account.getBalance() + "€");
+                    System.out.println("Il saldo disponibile ora è: " + df.format(account.getBalance()) + "€");
                 }
                 case 2 -> {
-                    System.out.println("Il tuo saldo attuale è: " + account.getBalance() + "€");
+                    System.out.println("Il tuo saldo attuale è: " + df.format(account.getBalance()) + "€");
                     System.out.println("Quanto vuoi prelevare?");
                     int withdraw = Integer.parseInt(scan.nextLine());
                     if (account.getBalance() - withdraw >= 0){
                         System.out.println("Caricamento in corso...");
                         System.out.println("Il tuo saldo è stato aggiornato");
                         account.setBalance(account.getBalance() - withdraw);
-                        System.out.println("Il saldo disponibile ora è: " + (account.getBalance()) + "€");
+                        System.out.println("Il saldo disponibile ora è: " + df.format(account.getBalance()) + "€");
                     } else {
                         System.out.println("Mi dispice non hai abbastanza disponibiltà");
                     }
--- src/org/lessons/java/shop/Main.java
+++ src/org/lessons/java/shop/Main.java
@@ -1,11 +1,25 @@
 package org.lessons.java.shop;
 
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        DecimalFormat df = new DecimalFormat("###.00");
+        Scanner scan = new Scanner(System.in);
+
         Product product = new Product();
-        product.setName("LG");
-        product.setDescription("Ottimo monitor, luminoso e ben definito");
-        product.setPrice(55);
-        System.out.println(product);
+        System.out.println("Che nome ha il tuo prodotto?: ");
+        product.setName(scan.nextLine());
+        System.out.println("Aggiungi una breve descrizione: ");
+        product.setDescription(scan.nextLine());
+        System.out.println("Qual è il prezzo?: ");
+        product.setPrice(Integer.parseInt(scan.nextLine()));
+
+        System.out.println("Codice Prodotto: " + product.getCode());
+        System.out.println("Il tuo prodotto è stato aggiunto con successo!");
+        System.out.println("Nome: " + product.getName());
+        System.out.println("Descrizione: " + product.getDescription());
+        System.out.println("Prezzo: " + df.format(product.getPrice()) + "€");
     }
 }
